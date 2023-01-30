1fde38f4eb5b064db01018fb3ec08049e9601afb
Plane <plane2001@outlook.com>
2023-01-30 17:53:33 +0530

    Added constants and theme

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,6 @@
+class AppwriteConstants {
+  static const String databaseId = "63d749570a4527739b68";
+  static const String projectId = "63d747ce99fce9855f00";
+  static const String endPoint =
+      "http://localhost:80/v1"; // End point helps flutter app communicate with appwrite
+}
